1d36c9fadd3ee0158f55f99c487820ebcb66375a
Patrick Woolard <73622620+JwoolardAU@users.noreply.github.com>
2022-03-20 19:50:01 -0400

    Updated main to allow for swarm control (temp)
    Old single drone control has been commented out. I will change it so that we can switch based on either some bool variable or a command line argument.

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ import configargparse
 import cv2 as cv
 
 from gestures.tello_gesture_controller import TelloGestureController
+from gestures.swarm_gesture_controller import SwarmGestureController
 from utils import CvFpsCalc
 
 from djitellopy import Tello
+from djitellopy import TelloSwarm
 from gestures import *
 
 import threading
@@ -63,27 +65,45 @@ def main():
     WRITE_CONTROL = False
     in_flight = False
 
-    # Camera preparation
-    tello = Tello()
-    tello.connect()
-    tello.streamon()
+    # Camera preparation - ORIGINAL (used for single drone)
+    # tello = Tello()
+    # tello.connect()
 
-    cap = tello.get_frame_read()
+    # Multi Drone Control - PATRICK (necessary to modify tello_gesture_controller)
+    Tello1_IP = "192.168.1.100"
+    Tello2_IP = "192.168.1.200"
+    drone1 = Tello(Tello1_IP)
+    drone2 = Tello(Tello2_IP)
+    swarm = TelloSwarm([drone1,drone2])
+    swarm.connect()
+    
+    # Uncomment to use tello's camera - ORIGINAL
+    # cap = tello.get_frame_read() 
+    # tello.streamon()
+
+    # Uncomment to use computer's camera - PATRICK
+    cap = cv.VideoCapture(0) 
 
     # Init Tello Controllers
-    gesture_controller = TelloGestureController(tello)
-    keyboard_controller = TelloKeyboardController(tello)
+    # gesture_controller = TelloGestureController(tello) # ORIGINAL
+    gesture_controller = SwarmGestureController(swarm) # PATRICK
+    # keyboard_controller = TelloKeyboardController(tello) # ORIGINAL
 
     gesture_detector = GestureRecognition(args.use_static_image_mode, args.min_detection_confidence,
                                           args.min_tracking_confidence)
     gesture_buffer = GestureBuffer(buffer_len=args.buffer_len)
 
-    def tello_control(key, keyboard_controller, gesture_controller):
+    def tello_control(key, keyboard_controller, gesture_controller): # ORIGINAL
         global gesture_buffer
 
         if KEYBOARD_CONTROL:
             keyboard_controller.control(key)
         else:
+            gesture_controller.gesture_control(gesture_buffer)
+
+    def swarm_control(gesture_controller): # PATRICK 
+        global gesture_buffer
+        if not KEYBOARD_CONTROL: # Wait to press G key
             gesture_controller.gesture_control(gesture_buffer)
 
     def tello_battery(tello):
@@ -100,9 +120,8 @@ def main():
     number = -1
     battery_status = -1
 
-    tello.move_down(20)
-
     while True:
+
         fps = cv_fps_calc.get()
 
         # Process Key (ESC: end)
@@ -112,12 +131,14 @@ def main():
         elif key == 32:  # Space
             if not in_flight:
                 # Take-off drone
-                tello.takeoff()
+                # tello.takeoff() # ORIGINAL
+                swarm.takeoff() # PATRICK
                 in_flight = True
 
             elif in_flight:
                 # Land tello
-                tello.land()
+                # tello.land() # ORIGINAL
+                swarm.land() # PATRICK
                 in_flight = False
 
         elif key == ord('k'):
@@ -138,24 +159,36 @@ def main():
                 number = key - 48
 
         # Camera capture
-        image = cap.frame
+        # image = cap.frame # ORIGINAL - Uncomment to use tello's camera 
+
+        # PATRICK - Uncomment below to use computer's camera
+        success, image = cap.read()
+        if not success:
+            continue # try to capture another frame successfully 
+
 
         debug_image, gesture_id = gesture_detector.recognize(image, number, mode)
         gesture_buffer.add_gesture(gesture_id)
 
-        # Start control thread
-        threading.Thread(target=tello_control, args=(key, keyboard_controller, gesture_controller,)).start()
-        threading.Thread(target=tello_battery, args=(tello,)).start()
+        # Start control threads
+        # threading.Thread(target=tello_control, args=(key, keyboard_controller, gesture_controller,)).start() # ORIGINAL
+        # threading.Thread(target=tello_battery, args=(tello,)).start() # ORIGINAL
+        threading.Thread(target=swarm_control, args=(gesture_controller,)).start() # PATRICK
 
         debug_image = gesture_detector.draw_info(debug_image, fps, mode, number)
 
         # Battery status and image rendering
-        cv.putText(debug_image, "Battery: {}".format(battery_status), (5, 720 - 5),
-                   cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        # cv.putText(debug_image, "Battery: {}".format(battery_status), (5, 720 - 5), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2) # ORIGINAL
         cv.imshow('Tello Gesture Recognition', debug_image)
 
-    tello.land()
-    tello.end()
+    # ORGINAL 
+    # tello.land()
+    # tello.end()
+    
+    # PATRICK
+    swarm.parallel(lambda i, tello: tello.land() )
+    swarm.end()
+    
     cv.destroyAllWindows()
 
 
