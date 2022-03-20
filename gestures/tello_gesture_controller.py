856b477b94e61ed0db8bd72a54d90976522dbe15
Patrick Woolard <73622620+JwoolardAU@users.noreply.github.com>
2022-03-20 19:54:37 -0400

    Added logic for drone flipping

--- gestures/tello_gesture_controller.py
+++ gestures/tello_gesture_controller.py
@@ -1,4 +1,5 @@
 from djitellopy import Tello
+import keyboard
 
 
 class TelloGestureController:
@@ -17,7 +18,18 @@ class TelloGestureController:
         print("GESTURE", gesture_id)
 
         if not self._is_landing:
-            if gesture_id == 0:  # Forward
+
+            # If keyboard chars are in conditionals
+            # they are for Patrick's special flight behavior
+
+            if gesture_id == 8 and keyboard.is_pressed('z'): # Flip
+                self.tello.flip("f")
+                print("Flip")
+
+            if gesture_id == 0 and keyboard.is_pressed('f'):  # Left Circle
+                self.yaw_velocity = 30 # turn left
+                self.forw_back_velocity = 20 # go forward at same speed of turn
+            elif gesture_id == 0:
                 self.forw_back_velocity = 30
             elif gesture_id == 1:  # STOP
                 self.forw_back_velocity = self.up_down_velocity = \
@@ -38,12 +50,15 @@ class TelloGestureController:
 
             elif gesture_id == 6: # LEFT
                 self.left_right_velocity = 20
+                #self.yaw_velocity = 50 # turn left
             elif gesture_id == 7: # RIGHT
                 self.left_right_velocity = -20
+                #self.yaw_velocity = -50 # turn right
 
             elif gesture_id == -1:
                 self.forw_back_velocity = self.up_down_velocity = \
                     self.left_right_velocity = self.yaw_velocity = 0
+
 
             self.tello.send_rc_control(self.left_right_velocity, self.forw_back_velocity,
                                        self.up_down_velocity, self.yaw_velocity)
